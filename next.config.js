9738b21f3b98cfc2b168ce154a7ed9c4e4da16b8
iMubarak Adebayo L <lefilsadebayo@gmail.com>
2025-12-04 13:44:44 -0800

    fix: bulletproof next.config.js – this one works forever

--- next.config.js
+++ next.config.js
@@ -1,1 +1,1 @@
-import { NextConfig } from "next"; const nextConfig = { reactStrictMode: true }; export default nextConfig; 
+/** @type {import('next').NextConfig} */ const nextConfig = { reactStrictMode: true }; export default nextConfig; 
